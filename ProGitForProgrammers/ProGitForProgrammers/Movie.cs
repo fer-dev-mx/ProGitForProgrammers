ee53970917c821f30c2de456c934e0bd49292a3e
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2021-11-29 10:46:23 -0600

    Add Movie Class with properties

---
+++ ProGitForProgrammers/ProGitForProgrammers/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProGitForProgrammers
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public List<string> Actors { get; set; }
+    }
+}
